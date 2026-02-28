d46de25c9500754e8eb1ec9703fabf534418b7b5
Sumit Kushwah <sumitkushwahji@gmail.com>
2026-02-28 11:31:10 +0530

    initial commit

---
+++ src/main/java/com/sumit/holy_attack/HolyAttackApplication.java
@@ -0,0 +1,13 @@
+package com.sumit.holy_attack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolyAttackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolyAttackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumit/holy_attack/HolyAttackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.holy_attack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolyAttackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
